5376ad398eb2895b5ad1ef70197e37149d067cf7
Stephen R Pietrowicz <srp@illinois.edu>
2020-08-28 15:29:55 -0500

    Changes necessary because of change in dm_csc_base to use BaseCSC

--- bin/run_cc_archiver_csc.py
+++ bin/run_cc_archiver_csc.py
@@ -3,5 +3,5 @@ import asyncio
 from lsst.dm.CCArchiver.ccarchiver_csc import CCArchiverCSC
 
 
-csc = CCArchiverCSC(index=None, schema_file='CCArchiver.yaml', initial_simulation_mode=False)
+csc = CCArchiverCSC(index=None)
 asyncio.get_event_loop().run_until_complete(csc.done_task)
